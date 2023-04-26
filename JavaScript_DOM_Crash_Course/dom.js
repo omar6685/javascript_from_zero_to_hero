89f0d577bd8694118ae71ddb691d33d176ab2f37
omar6685 <omr6685@hotmail.com>
2023-04-26 14:41:53 +0300

    Document Object

---
+++ JavaScript_DOM_Crash_Course/dom.js
@@ -0,0 +1,13 @@
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all[10]);
+// document.all[10].textContant = 'text';
+// console.log(document.forms);
+// console.log(document.links);
