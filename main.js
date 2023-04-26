3dbc4351b1a9b498ac01af750786d1d664b4ab09
omar6685 <omr6685@hotmail.com>
2023-04-26 13:47:10 +0300

    events

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-const ul = document.querySelector('.items');
-
-
-// ul.remove();
-
-// ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'hello';
-ul.children[1].innerText = 'hello';
-ul.lastElementChild.innerHTML = '<h4>Hello</h4';
-
-
 const btn = document.querySelector('.btn');
-btn.style.background = 'red';
+
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background = '#000';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>hello</h1>';
+
+});
