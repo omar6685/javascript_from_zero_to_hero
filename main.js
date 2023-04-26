7802a212fcb9a59a6dfa4c1caa2ab9e51abdb533
omar6685 <omr6685@hotmail.com>
2023-04-26 11:48:07 +0300

    Loops

--- main.js
+++ main.js
@@ -16,7 +16,33 @@ const todos = [
     }
 ]
 
-console.log(todos[1].text)
+for(let i = 0; i < 10; i++) {
+    console.log(`for loop number ${i}`);
+}
 
-const todoJSON = JSON.stringify(todos);
-console.log(todoJSON)
+
+let i = 0;
+while(i < 10) {
+    console.log(`while loop number ${i}`);
+    i++;
+}
+
+for(let i = 0; i < todos.length; i++) {
+    console.log(todos[i].text);
+}
+
+for(let todo of todos) {
+    console.log(todo.text);
+}
+
+todos.forEach(function(todo) {
+    console.log(todo.id);
+});
+
+const todoCompleted = todos.filter(function(todo) {
+    return todo.isCompleted == true;
+}).map(function(todo) {
+    return  todo.text; 
+})
+
+console.log(todoCompleted);
