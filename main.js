65e4033757685dfce2aa6cf3f1d231db86fde493
omar6685 <omr6685@hotmail.com>
2023-04-26 07:50:53 +0300

    string 2

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const name = 'john';
-const age = 20;
+const s= 'hello world';
+console.log(s.length);
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.substring(0, 5));
+console.log(s.substring(0, 5).toLocaleUpperCase());
 
-const hello = `my is ${name} and I am ${age}`;
-console.log(hello);
+const a = 'omar , ali, ahmed'
+console.log(a.split(', '))
+
