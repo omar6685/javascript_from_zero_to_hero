d456168521be323015186bfb1afbc4cd4abf979e
omar6685 <omr6685@hotmail.com>
2023-04-27 07:59:55 +0300

    events

--- JavaScript_DOM_Crash_Course/dom.js
+++ JavaScript_DOM_Crash_Course/dom.js
@@ -73,7 +73,7 @@
 
 
 
-var itemList = document.querySelector('#items');
+// var itemList = document.querySelector('#items');
 // console.log(itemList.parentNode);
 // itemList.parentNode.style.backgroundColor = '#f4f4f4';
 // console.log(itemList.parentNode.parentNode);
@@ -122,16 +122,53 @@ var itemList = document.querySelector('#items');
 
 // container.insertBefore(newDiv, h1);
 
-var button = document.getElementById('button').addEventListener
-('click', buttonClick);
+// var button = document.getElementById('button').addEventListener
+// ('click', buttonClick);
 
-function buttonClick(e) {
+// function buttonClick(e) {
 //     document.getElementById('header-title').textContent = 'title' ;
 //     document.querySelector('#main').style.backgroundColor = '#f4f4f4';
 //   console.log(e);
-     console.log(e.target);
-     console.log(e.target.id);
-     console.log(e.target.className);
-     var output = document.getElementById('output');
-     output.innerHTML = '<h3>'+e.target.id+'</h3>';
-}
+    //  console.log(e.target);
+    //  console.log(e.target.id);
+    //  console.log(e.target.className);
+    //  var output = document.getElementById('output');
+    //  output.innerHTML = '<h3>'+e.target.id+'</h3>';
+    //  console.log(e.type);
+    //  console.log(e.clientY);
+    //  console.log(e.offsetX);
+    //  console.log(e.altKey);
+// }
+
+// var button = document.getElementById('button');
+// var box = document.getElementById('box');
+// button.addEventListener('click', runEvent);
+// button.addEventListener('dblclick', runEvent);
+// button.addEventListener('mouseover', runEvent);
+// button.addEventListener('mousedown', runEvent);
+
+// box.addEventListener('mouseenter', runEvent);
+// box.addEventListener('mouseleave', runEvent);
+// box.addEventListener('mouseover', runEvent);
+// box.addEventListener('mouseout', runEvent);
+
+// box.addEventListener('mousemove', runEvent);
+
+
+// var itemInput = document.querySelector('input[type="text"]');
+// var form = document.querySelector('form');
+
+// itemInput.addEventListener('keydown', runEvent)
+
+// itemInput.addEventListener('focus', runEvent);
+
+// function runEvent(e) {
+
+//     console.log('event type : ' + e.type);
+
+//     console.log(e.target.value);
+    // output.innerHTML = '<h3>MouseX: ' + e.offsetX + '</h3><h3>MouseY: ' + e.offsetY + '</h3>';
+
+//    document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",40)";
+
+// }
