258bf28d4af67f2472b0fa1ed635bef2709d2ef5
omar6685 <omr6685@hotmail.com>
2023-04-27 11:51:57 +0300

    prototypes

--- JavaScript _OOP_Crash_Course/main.js
+++ JavaScript _OOP_Crash_Course/main.js
@@ -1,13 +1,42 @@
 // console.log(navigator.appVersion);
 
+// function Book(title, author, year) {
+//     this.title = title;
+//     this.author = author;
+//     this.year = year;
+//     this.getSummary = function(){
+//         return `${this.title} - ${this.author} - ${this.year}`;
+//     }
+// }
+
+
+// const book1 = new Book(
+//     "The Hobbit",
+//     "J.R.R. Tolkien",
+//     '1984'
+// );
+// console.log(book1.getSummary())
+
 function Book(title, author, year) {
     this.title = title;
     this.author = author;
     this.year = year;
-    this.getSummary = function(){
-        return `${this.title} - ${this.author} - ${this.year}`;
-    }
 }
+
+
+Book.prototype.getSummary = function() {
+    return `${this.title} - ${this.author} - ${this.year}`;
+}
+
+Book.prototype.getAge = function() {
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} years old`;
+}
+
+Book.prototype.revise = function(newYear) {
+    this.year = newYear;
+    this.revised = true;
+};
 
 
 const book1 = new Book(
@@ -15,4 +44,6 @@ const book1 = new Book(
     "J.R.R. Tolkien",
     '1984'
 );
-console.log(book1.getSummary())
+console.log(book1.getAge())
+book1.revise('2009')
+console.log(book1)
