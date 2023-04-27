cc780faf1bb30fb8f793a6499cadebf717b0b2f0
omar6685 <omr6685@hotmail.com>
2023-04-27 11:37:01 +0300

    constructor

--- JavaScript _OOP_Crash_Course/main.js
+++ JavaScript _OOP_Crash_Course/main.js
@@ -1,2 +1,18 @@
 // console.log(navigator.appVersion);
 
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.getSummary = function(){
+        return `${this.title} - ${this.author} - ${this.year}`;
+    }
+}
+
+
+const book1 = new Book(
+    "The Hobbit",
+    "J.R.R. Tolkien",
+    '1984'
+);
+console.log(book1.getSummary())
