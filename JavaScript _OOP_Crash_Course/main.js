20f17c11fa810dcb768aaed9a3b4ae0b48b6b639
omar6685 <omr6685@hotmail.com>
2023-04-27 12:07:20 +0300

    inheritence

--- JavaScript _OOP_Crash_Course/main.js
+++ JavaScript _OOP_Crash_Course/main.js
@@ -17,6 +17,38 @@
 // );
 // console.log(book1.getSummary())
 
+// function Book(title, author, year) {
+//     this.title = title;
+//     this.author = author;
+//     this.year = year;
+// }
+
+
+// Book.prototype.getSummary = function() {
+//     return `${this.title} - ${this.author} - ${this.year}`;
+// }
+
+// Book.prototype.getAge = function() {
+//     const years = new Date().getFullYear() - this.year;
+//     return `${this.title} is ${years} years old`;
+// }
+
+// Book.prototype.revise = function(newYear) {
+//     this.year = newYear;
+//     this.revised = true;
+// };
+
+
+// const book1 = new Book(
+//     "The Hobbit",
+//     "J.R.R. Tolkien",
+//     '1984'
+// );
+// console.log(book1.getAge())
+// book1.revise('2009')
+// console.log(book1)
+
+
 function Book(title, author, year) {
     this.title = title;
     this.author = author;
@@ -28,22 +60,12 @@ Book.prototype.getSummary = function() {
     return `${this.title} - ${this.author} - ${this.year}`;
 }
 
-Book.prototype.getAge = function() {
-    const years = new Date().getFullYear() - this.year;
-    return `${this.title} is ${years} years old`;
+
+function Magazine(title, author, year, month) {
+    Book.call(this, title, author, year);
+    this.month = month;
 }
 
-Book.prototype.revise = function(newYear) {
-    this.year = newYear;
-    this.revised = true;
-};
+const mag1 = new Magazine('mag one', 'John Smith', '2018', 'Jon');
 
-
-const book1 = new Book(
-    "The Hobbit",
-    "J.R.R. Tolkien",
-    '1984'
-);
-console.log(book1.getAge())
-book1.revise('2009')
-console.log(book1)
+console.log(mag1)
