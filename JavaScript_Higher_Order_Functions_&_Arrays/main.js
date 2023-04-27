1c35d8295ae68c5ada22500a433a8ce24fa4c0fa
omar6685 <omr6685@hotmail.com>
2023-04-27 09:24:49 +0300

    forEach loop

--- JavaScript_Higher_Order_Functions_&_Arrays/main.js
+++ JavaScript_Higher_Order_Functions_&_Arrays/main.js
@@ -14,7 +14,11 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
   
 
 
-for(let i = 0; i < companies.length; i++){
-    console.log(companies[i]);
-}
+// for(let i = 0; i < companies.length; i++){
+//     console.log(companies[i]);
+// }
 
+companies.forEach(function(Company){
+
+    console.log(Company);
+});
