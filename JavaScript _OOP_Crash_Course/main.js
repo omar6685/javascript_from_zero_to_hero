3730a5e249b9ece60e9811320062f0c56239fa00
omar6685 <omr6685@hotmail.com>
2023-04-27 12:13:39 +0300

    object_create

--- JavaScript _OOP_Crash_Course/main.js
+++ JavaScript _OOP_Crash_Course/main.js
@@ -49,23 +49,48 @@
 // console.log(book1)
 
 
-function Book(title, author, year) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
+// function Book(title, author, year) {
+//     this.title = title;
+//     this.author = author;
+//     this.year = year;
+// }
+
+
+// Book.prototype.getSummary = function() {
+//     return `${this.title} - ${this.author} - ${this.year}`;
+// }
+
+
+// function Magazine(title, author, year, month) {
+//     Book.call(this, title, author, year);
+//     this.month = month;
+// }
+
+// const mag1 = new Magazine('mag one', 'John Smith', '2018', 'Jon');
+
+// console.log(mag1)
+
+
+const bookProtos = {
+    getSummary: function() {
+        return `${this.title} - ${this.author} - ${this.year}`;
+    },
+    getAge: function() {
+        const years = new Date().getFullYear() - this.year;
+        return `${this.title} is ${years} years old`;
+    }
 }
 
+// const book1 = Object.create(bookProtos);
+// book1.title = 'The Hobbit';
+// book1.author = 'J.R.R. Tolkien';
+// book1.year = '1984';
 
-Book.prototype.getSummary = function() {
-    return `${this.title} - ${this.author} - ${this.year}`;
-}
 
+const book1 = Object.create(bookProtos,{
+    title: {value: 'The Hobbit'},
+    author: {value: 'J.R.R. Tolkien'},
+    year: {value: '1984'}
+});
 
-function Magazine(title, author, year, month) {
-    Book.call(this, title, author, year);
-    this.month = month;
-}
-
-const mag1 = new Magazine('mag one', 'John Smith', '2018', 'Jon');
-
-console.log(mag1)
+console.log(book1)
