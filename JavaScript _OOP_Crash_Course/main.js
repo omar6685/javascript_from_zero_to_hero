09132a8dc411d8cd7c8d601dd599dd04482d6a0a
omar6685 <omr6685@hotmail.com>
2023-04-27 12:49:42 +0300

    subclasses

--- JavaScript _OOP_Crash_Course/main.js
+++ JavaScript _OOP_Crash_Course/main.js
@@ -96,6 +96,31 @@
 // console.log(book1)
 
 
+// class Book {
+//     constructor(title, author, year) {
+//         this.title = title;
+//         this.author = author;
+//         this.year = year;
+//     }
+//     getSummary() {
+//         return `${this.title} - ${this.author} - ${this.year}`;
+//     }
+//     getAge() {
+//         const years = new Date().getFullYear() - this.year;
+//         return `${this.title} is ${years} years old`;
+//     }
+//     revise(newYear) {
+//         this.year = newYear;
+//         this.revised = true;
+//     }
+// }
+
+// const book1 = new Book('The Hobbit', 'omar', '2001');
+
+// console.log(book1)
+
+
+
 class Book {
     constructor(title, author, year) {
         this.title = title;
@@ -105,16 +130,17 @@ class Book {
     getSummary() {
         return `${this.title} - ${this.author} - ${this.year}`;
     }
-    getAge() {
-        const years = new Date().getFullYear() - this.year;
-        return `${this.title} is ${years} years old`;
-    }
-    revise(newYear) {
-        this.year = newYear;
-        this.revised = true;
+
+}
+
+
+class Magazine extends Book {
+    constructor(title, author, year, month) {
+        super(title, author, year);
+        this.month = month;
     }
 }
 
-const book1 = new Book('The Hobbit', 'omar', '2001');
+const mag1 = new Magazine('mag one', 'John Smith', '2018', 'Jon');
 
-console.log(book1)
+console.log(mag1.getSummary())
